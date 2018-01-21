b390f155158364f590e7a6d9250b120a86f9d68d
Maxwin-z <zwd2005@gmail.com>
2018-01-21 15:08:52 +0800

    update url

--- happy123/prod.js
+++ happy123/prod.js
@@ -1,13 +1,14 @@
+const prev = 'https://maxwin-z.github.io/happy123/'
 ;['common.js', 'index.js'].forEach(src => {
   const script = document.createElement('script')
-  script.src = `https://mysql.byutech.cn/${src}`
+  script.src = `${prev}${src}`
   console.log(`append ${src}`)
   document.querySelector('head').appendChild(script)
 })
 ;['index.css'].forEach(css => {
   const link = document.createElement('link')
   link.setAttribute('rel', 'stylesheet')
-  link.setAttribute('href', `https://mysql.byutech.cn/${css}`)
+  link.setAttribute('href', `${prev}${css}`)
   document.querySelector('head').appendChild(link)
 })
-console.log('in dev.js')
+console.log('in prod.js')
